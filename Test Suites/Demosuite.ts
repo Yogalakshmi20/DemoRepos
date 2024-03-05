65a9d4fd678300c400a636f74dcc94a48d7c0b89
Yogalakshmi200398 <158251503+YogalakshmiOptisol@users.noreply.github.com>
2024-03-05 11:41:18 +0530

    Second commit

---
+++ Test Suites/Demosuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demosuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>96aa749e-3dc6-4b1b-8538-b5c6035ad955</testSuiteGuid>
+   <testCaseLink>
+      <guid>7df8779e-1ea9-4830-98da-f5051f7d386e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
